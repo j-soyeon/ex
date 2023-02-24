90658885a6cb4a3babfc6dd9217f409210423d7d
yeon95 <124026878+yeon95@users.noreply.github.com>
2023-02-24 02:59:32 +0000

    카운팅 추가

--- js/payco.js
+++ js/payco.js
@@ -1,43 +1,43 @@
 
-    var swiper = new Swiper(".mySwiper", {
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
 
-    
+
 $('.btn li').click(function(){
-  $(this).addClass('active')
-  $(this).siblings().removeClass('active')
+$(this).addClass('active')
+$(this).siblings().removeClass('active')
 
-  var con = $(this).attr('data-alt')
-  // $('.tabs .tab_con').removeClass('active')
-  $('#' + con).addClass('active').siblings().removeClass('active')
-  return false;
+var con = $(this).attr('data-alt')
+// $('.tabs .tab_con').removeClass('active')
+$('#' + con).addClass('active').siblings().removeClass('active')
+return false;
 });
 
 $('.sub_btn li') .click(function(){
-  $(this).addClass('active')
-  $(this).siblings().removeClass('active')
+$(this).addClass('active')
+$(this).siblings().removeClass('active')
 
-  var subCon = $(this).attr('data-txt')
-  // $('.sub_tab .sub_con').removeClass('active')
-  $('#' + subCon).addClass('active').siblings().removeClass('active')
-  return false;
+var subCon = $(this).attr('data-txt')
+// $('.sub_tab .sub_con').removeClass('active')
+$('#' + subCon).addClass('active').siblings().removeClass('active')
+return false;
 });
 console.log()
 
 
 
-    let countBox = document.querySelector('.count'),
-        count = 0;
+let countBox = document.querySelector('.count'),
+    count = 0;
 
-    let counting = setInterval(function () {
-        if (count == 12000000) {
-            clearInterval(counting);
-            return false;
-        }
-        count += 10000;
-        countBox.innerHTML = new Intl.NumberFormat().format(count);
-    }, 5)
+let counting = setInterval(function () {
+    if (count == 12000000) {
+        clearInterval(counting);
+        return false;
+    }
+    count += 10000;
+    countBox.innerHTML = new Intl.NumberFormat().format(count);
+}, 5)
