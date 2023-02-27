180e7eddf19c665f25d74cc4ff76a5034b1baf79
yeon95 <124026878+yeon95@users.noreply.github.com>
2023-02-27 05:42:35 +0000

    Update payco.js

--- js/payco.js
+++ js/payco.js
@@ -1,16 +1,11 @@
+
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 1,
-  spaceBetween: 30,
-  loop: true,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 });
+
 
 $('.btn li').click(function(){
 $(this).addClass('active')
